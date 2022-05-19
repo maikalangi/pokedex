475e7acaf3a595e349bfde1e9a3ab288cbf33c5a
maikalangi <maikalangi@gmail.com>
2022-05-19 15:19:34 -0700

    commits all INDUCES routes and middleware

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // DEPENDENCIES
 const express = require('express');
 const pokemon = require('./models/pokemon');
-const morgan = require('morgan');
-const methodOverride = require('method-override');
+const morgan = require('./.gitignore/node_modules/morgan');
+const methodOverride = require('./.gitignore/node_modules/method-override');
+const res = require('express/lib/response');
 
 // APPLICATION INITIALIZATION
 const app = express();
@@ -18,18 +19,45 @@ app.use(morgan('dev'));
 // =========ROUTES===============
 
 // INDEX
-
-// NEW
-
-// DELETE
-
-// UPDATE
-
-// CREATE
-
-// EDIT
-
+app.get('/', (req, res)=>{
+    res.render('index.ejs', { pokemon });
+});
 // SHOW
+app.get('/:id/', (req, res)=>{
+    res.render('show.ejs', {
+        pokemon: pokemon[req.params.id]
+    });
+});
+// NEW
+app.get('/new/', (req, res)=>{
+    res.render('new.ejs', { pokemon });
+});
+// EDIT
+app.get('/:id/edit', (req, res)=>{
+    res.render(
+        'edit.ejs',
+        {
+            pokemon: pokemon[req.params.id],
+            index: req.params.id,
+        }
+    );
+});
+// CREATE
+app.post('/', (req, res)=>{
+    pokemon.push(req.body);
+    res.redirect('/');
+});
+// UPDATE
+app.put('/:id', (req, res)=>{
+    // form
+    pokemon[req.params.id] = req.body; // change selected value data
+    res.redirect('/');
+});
+// DELETE
+app.delete('/:id', (req, res)=>{
+    pokemon.splice(req.params.id, 1);
+    res.redirect('/');
+});
 
 // REQUEST LISTENER
 app.listen(port, ()=>{
