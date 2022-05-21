a00762cc7ae38cb4f22b558edb2ffecf67dafd2a
maikalangi <maikalangi@gmail.com>
2022-05-21 15:26:22 -0700

    turns .gitignore from directory to file; edits partials

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // DEPENDENCIES
 const express = require('express');
 const pokemon = require('./models/pokemon');
-const morgan = require('./.gitignore/node_modules/morgan');
-const methodOverride = require('./.gitignore/node_modules/method-override');
+const morgan = require('./node_modules/morgan');
+const methodOverride = require('./node_modules/method-override');
 const res = require('express/lib/response');
 
 // APPLICATION INITIALIZATION
@@ -13,38 +13,56 @@ const port = 3000;
 
 // MIDDLEWARE
 app.use(express.urlencoded({ extended: false }));
+
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
 
 // =========ROUTES===============
 
+// DATA CHECK
+app.get('/data/', (req, res)=>{
+    res.send(pokemon);
+});
+
 // INDEX
 app.get('/', (req, res)=>{
-    res.render('index.ejs', { pokemon });
-});
-// SHOW
-app.get('/:id/', (req, res)=>{
-    res.render('show.ejs', {
-        pokemon: pokemon[req.params.id]
+    res.render('index.ejs', { 
+        pokemon,
+        tabTitle: 'Pokedex',
     });
 });
 // NEW
 app.get('/new/', (req, res)=>{
-    res.render('new.ejs', { pokemon });
+    res.render('new.ejs', { 
+        pokemon,
+        tabTitle: 'New Entry',
+    });
+});
+// SHOW
+app.get('/:id/', (req, res)=>{
+    // console.log(pokemon[req.params.id])
+    res.render('show.ejs', {
+        pokemon: pokemon[req.params.id],
+        tabTitle: pokemon[req.params.id],
+    });
 });
 // EDIT
 app.get('/:id/edit', (req, res)=>{
+    // console.log(pokemon[req.params.id]);
     res.render(
         'edit.ejs',
         {
             pokemon: pokemon[req.params.id],
             index: req.params.id,
+            tabTitle: pokemon[req.params.id] + ' Edit Page',
         }
     );
+    // console.log(pokemon[req.params.id]);
 });
 // CREATE
 app.post('/', (req, res)=>{
     pokemon.push(req.body);
+    console.log(req.body);
     res.redirect('/');
 });
 // UPDATE
@@ -58,6 +76,8 @@ app.delete('/:id', (req, res)=>{
     pokemon.splice(req.params.id, 1);
     res.redirect('/');
 });
+
+app.use(express.static('./public'));
 
 // REQUEST LISTENER
 app.listen(port, ()=>{
