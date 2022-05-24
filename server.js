9110922404c9a9dd1dc2ea7239891878355af210
maikalangi <maikalangi@gmail.com>
2022-05-23 22:46:54 -0700

    ammends errors with create new function and styles app

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.get('/pokemon/', (req, res)=>{
         pokemon,
         tabTitle: 'Pokedex',
     });
+    console.log(pokemon[0], pokemon[1]);
 });
 
 // NEW
@@ -53,7 +54,7 @@ app.delete('/pokemon/:id', (req, res)=>{
 app.put('/pokemon/:id', (req, res)=>{
    console.log(req.body.type);
     pokemon[req.params.id].name=req.body.name;
-    pokemon[req.params.id].type=req.body.type;
+    pokemon[req.params.id].type=`${req.body.type}`;
     pokemon[req.params.id].stats.hp=req.body.hp;
     pokemon[req.params.id].stats.attack=req.body.attack;
     pokemon[req.params.id].stats.defense=req.body.defense;
@@ -64,7 +65,6 @@ app.put('/pokemon/:id', (req, res)=>{
 // CREATE
 app.post('/pokemon/', (req, res)=>{
     pokemon.unshift(req.body);
-    console.log(req.body);
     res.redirect('/pokemon/');
 });
 
@@ -82,10 +82,9 @@ app.get('/pokemon/:id/edit', (req, res)=>{
 
 // SHOW
 app.get('/pokemon/:id/', (req, res)=>{
-    console.log(pokemon[req.params.id].name);
     res.render('show.ejs', {
         pokemon: pokemon[req.params.id],
-        tabTitle: pokemon[req.params.id].name,
+        tabTitle: pokemon[req.params.id].name
     });
 });
 
